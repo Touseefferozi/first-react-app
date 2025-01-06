695959f91ca0b7ab5fcd81bd2bc1e596f69dc6dc
Touseefferozi <touseefferozi145@gmail.com>
2025-01-06 20:39:36 +0500

    Navbar responsive toggle change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsNavOpen((prev) => !prev);
+  };
+
   return (
     <>
       {/* Navbar */}
@@ -9,14 +15,16 @@ function Navbar() {
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
             {/* Logo */}
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Navbar</span>
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              Navbar
+            </span>
           </a>
           <button
-            data-collapse-toggle="navbar-default"
+            onClick={handleToggle}
             type="button"
             className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
             aria-controls="navbar-default"
-            aria-expanded="false"
+            aria-expanded={isNavOpen}
           >
             <span className="sr-only">Open main menu</span>
             <svg
@@ -35,7 +43,12 @@ function Navbar() {
               />
             </svg>
           </button>
-          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+          <div
+            className={`${
+              isNavOpen ? 'block' : 'hidden'
+            } w-full md:block md:w-auto`}
+            id="navbar-default"
+          >
             <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
                 <Link
@@ -62,7 +75,6 @@ function Navbar() {
                   Services
                 </Link>
               </li>
-             
               <li>
                 <Link
                   to="/contact"
